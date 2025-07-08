57b4039b6a6cc74035a478573b2784062c735921
rafaelmm16 <rafael.merlomendes@gmail.com>
2025-07-08 08:55:34 -0300

    Remove tslib and related dependencies from the project

--- my-app/src/app/components/footer.tsx
+++ my-app/src/app/components/footer.tsx
@@ -5,7 +5,6 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-4 mt-8">
             <div className="container mx-auto text-center">
-                <p>© 2025 CRUD. All rights reserved.</p>
                 <div className="flex justify-center">
                     <Button />
                 </div>
